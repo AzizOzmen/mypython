fcb8ff0984c748879397e0662ece724649f70f0d
earslan <earslan3e@gmail.com>
2021-04-21 16:26:37 +0200

    21.04.2021

---
+++ best008/pynative/inputs/exercise7.py
@@ -0,0 +1,6 @@
+# Enter three string jhon emma kelly
+# Result: jhon emma kelly
+
+# Answer
+str1, str2, str3 = input("Enter three string: ").split("+")
+print(str1, str2, str3)
---
+++ best008/recap/deneme.py
@@ -0,0 +1,7 @@
+print('Ben', 25, 'California', sep='--')
+print('[%c]' % 65)
+x= '(%c)' % (255)
+print(type(x))
+
+x = float('NaN')
+print('%f, %e, %F, %E' % (x, x, x, x))
