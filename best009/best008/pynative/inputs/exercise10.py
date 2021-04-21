e3c9baf0f4ecec2833be26d2f680d902c1645b29
earslan <earslan3e@gmail.com>
2021-04-21 17:08:04 +0200

    copyalar silindi

--- best009/best008/pynative/inputs/exercise10.py
+++
@@ -1,5 +0,0 @@
-# Read line number 4 from the following file
-
-with open("test.txt", "r") as fp:
-    lines = fp.readlines()
-    print(lines[3])
--- best009/best008/pynative/inputs/exercise7.py
+++
@@ -1,6 +0,0 @@
-# Enter three string jhon emma kelly
-# Result: jhon emma kelly
-
-# Answer
-str1, str2, str3 = input("Enter three string: ").split("+")
-print(str1, str2, str3)
