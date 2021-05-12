c6d56a7e24df4199f72ab40bd405885f02f9ce59
earslan <earslan3e@gmail.com>
2021-05-12 18:12:27 +0200

    new assignments

---
+++ assigments/front_back_chair.py
@@ -0,0 +1,15 @@
+# Assignment - 5 (Front-Back Char)
+# Complete the Code Challenge question. Good luck!
+
+# 💡 Objective :
+# To improve your defining function and string skills.
+
+# Test	Result
+# print(front_back('clarusway'))
+# ylaruswac
+# print(front_back('a'))
+# a
+# print(front_back('ab'))
+# ba
+
+def front_back(word):
